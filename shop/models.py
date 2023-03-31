feb86414745b2959bbd06de697c65d77ef7cfad0
Meen Philip <meenphilip@gmail.com>
2023-03-31 23:32:12 +0300

    Format code with black

--- shop/models.py
+++ shop/models.py
@@ -3,34 +3,35 @@ from django.db import models
 
 # Create catalogue (category)
 class Category(models.Model):
-  name = models.CharField(max_length=200,db_index=True)
-  slug = models.SlugField(max_length=200,unique=True)
-  
-  class Meta:
-    ordering = ['name']
-    verbose_name = 'category'
-    verbose_name_plural = 'categories'
-  
-  def __str__(self):
-      return self.name
-    
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
 
-# Create Product 
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+# Create Product
 class Product(models.Model):
-  category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
-  name = models.CharField(max_length=200,db_index=True)
-  slug = models.SlugField(max_length=200,unique=True)
-  image = models.ImageField( upload_to='products/%Y/%m/%d',blank=True,null=True)
-  description =  models.TextField(blank=True,null=True)
-  price = models.DecimalField( max_digits=10, decimal_places=2)
-  available = models.BooleanField(default=True)
-  created = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(auto_now=True)
-  
-  class Meta:
-    ordering =['name']
-    index_together = (('id','slug'),)
-    
-  def __str__(self):
-      return self.name
-    
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["name"]
+        index_together = (("id", "slug"),)
+
+    def __str__(self):
+        return self.name
