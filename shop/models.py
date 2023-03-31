81c32aa962fee8feb23943b29506a9ee412cadc8
Meen Philip <meenphilip@gmail.com>
2023-03-31 22:36:15 +0300

    Add blank & null attribute to image field

--- shop/models.py
+++ shop/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
   category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
   name = models.CharField(max_length=200,db_index=True)
   slug = models.SlugField(max_length=200,unique=True)
-  image = models.ImageField( upload_to='products/%Y/%m/%d')
+  image = models.ImageField( upload_to='products/%Y/%m/%d',blank=True,null=True)
   description =  models.TextField(blank=True,null=True)
   price = models.DecimalField( max_digits=10, decimal_places=2)
   available = models.BooleanField(default=True)
