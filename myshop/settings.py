258e7e671e9b50beb770b1a4d2a398d526d03c30
Meen Philip <meenphilip@gmail.com>
2023-04-03 01:39:28 +0300

    Add cart app & cart session id.

--- myshop/settings.py
+++ myshop/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# set cart session key
+CART_SESSION_ID = "cart"
+
 
 # Application definition
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shop.apps.ShopConfig",
+    "cart.apps.CartConfig",
 ]
 
 MIDDLEWARE = [
