a6656a72b4c30e6a081a68d7005cb5734cfa6725
Meen Philip <meenphilip@gmail.com>
2023-04-03 22:45:13 +0300

    Add an Add to cart button to product detail page

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from cart.forms import CartAddProductForm
 
 
 # Get & display Available products view.
@@ -20,4 +21,17 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
 
     context = {"product": product}
+    return render(request, "shop/product/detail.html", context)
+
+
+# Add an Add to cart button to product detail page.
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+
+    cart_product_form = CartAddProductForm()
+    context = {
+        "product": product,
+        "cart_product_form": cart_product_form,
+    }
+
     return render(request, "shop/product/detail.html", context)
